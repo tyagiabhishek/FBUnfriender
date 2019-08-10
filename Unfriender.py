bb01a7c4664dcccc0fad4081a1222a6c552d13b6
Cheran <cheran.v.senthil@gmail.com>
2019-08-10 20:28:26 +0530

    Format with black and sort imports

--- Unfriender.py
+++ Unfriender.py
@@ -1,41 +1,45 @@
-from selenium import webdriver
-from time import sleep
-from selenium.common.exceptions import NoSuchElementException
-import pickle
 import getpass
+import pickle
+from time import sleep
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
 SCROLL_PAUSE_TIME = 5
 
 exec_path = input("Enter executable path for geckodriver: ")
-driver = webdriver.Firefox(executable_path='D:\\geckodriver-v0.24.0-win64\\geckodriver.exe')
-driver.get('https://www.facebook.com/')
+driver = webdriver.Firefox(
+    executable_path="D:\\geckodriver-v0.24.0-win64\\geckodriver.exe"
+)
+driver.get("https://www.facebook.com/")
 
-username_box = driver.find_element_by_id('email') 
+username_box = driver.find_element_by_id("email")
 email = input("Enter email: ")
-username_box.send_keys(email) 
-print ("Email Id entered") 
-sleep(1) 
-  
-password_box = driver.find_element_by_id('pass') 
-pwd = getpass.getpass(promt='Your password: ')
-password_box.send_keys(pwd) 
-print ("Password entered") 
-  
-login_box = driver.find_element_by_id('loginbutton') 
-login_box.click() 
+username_box.send_keys(email)
+print("Email Id entered")
+sleep(1)
+
+password_box = driver.find_element_by_id("pass")
+pwd = getpass.getpass(promt="Your password: ")
+password_box.send_keys(pwd)
+print("Password entered")
+
+login_box = driver.find_element_by_id("loginbutton")
+login_box.click()
 sleep(2)
 
 try:
-    code_box = driver.find_element_by_id('approvals_code')
-    login_code = getpass.getpass(promt='Authentication Code: ')
+    code_box = driver.find_element_by_id("approvals_code")
+    login_code = getpass.getpass(promt="Authentication Code: ")
     code_box.send_keys(login_code)
     print("login code sent")
-    submit_box = driver.find_element_by_id('checkpointSubmitButton')
+    submit_box = driver.find_element_by_id("checkpointSubmitButton")
     submit_box.click()
     sleep(1)
-    options = driver.find_elements_by_name('name_action_selected')
+    options = driver.find_elements_by_name("name_action_selected")
     options[1].click()
     sleep(1)
-    submit_box = driver.find_element_by_id('checkpointSubmitButton')
+    submit_box = driver.find_element_by_id("checkpointSubmitButton")
     submit_box.click()
     sleep(1)
 except NoSuchElementException:
@@ -43,7 +47,7 @@ except NoSuchElementException:
 
 real_friends = []
 try:
-    pickle_in = open("real_friends.pickle","rb")
+    pickle_in = open("real_friends.pickle", "rb")
     real_friends = pickle.load(pickle_in)
 except IOError:
     print("Pickle not present")
@@ -51,7 +55,7 @@ except IOError:
 if len(real_friends) == 0:
     User = input("Enter your first name: ")
     Profile = driver.find_element_by_link_text(User)
-    Profile = Profile.get_attribute('href')
+    Profile = Profile.get_attribute("href")
     driver.get(Profile)
     sleep(1)
     print("Switched to profile")
@@ -63,8 +67,8 @@ if len(real_friends) == 0:
     sleep(SCROLL_PAUSE_TIME)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     sleep(SCROLL_PAUSE_TIME)
-    All_friends = driver.find_element_by_link_text('Friends')
-    All_friends = All_friends.get_attribute('href')
+    All_friends = driver.find_element_by_link_text("Friends")
+    All_friends = All_friends.get_attribute("href")
     driver.get(All_friends)
     sleep(1)
 
@@ -91,42 +95,41 @@ if len(real_friends) == 0:
     for friends_div in friends_divs:
         friends_as = friends_div.find_elements_by_tag_name("a")
         for friends_a in friends_as:
-            friends.append(friends_a.get_attribute('href'))
+            friends.append(friends_a.get_attribute("href"))
 
-    print("Total Friends found: "+str(len(friends)))
-
-
+    print("Total Friends found: " + str(len(friends)))
 
     count = 0
     for friend in friends:
-        if count<1500:
-            count = count+1
+        if count < 1500:
+            count = count + 1
             continue
-        if(friend.find("friends_tab") != -1):
+        if friend.find("friends_tab") != -1:
             real_friends.append(friend)
-    
-    pickle_out = open("real_friends.pickle","wb")
-    pickle.dump(real_friends,pickle_out)
+
+    pickle_out = open("real_friends.pickle", "wb")
+    pickle.dump(real_friends, pickle_out)
 
 for friend in real_friends:
     driver.get(friend)
     try:
-        elem = driver.find_element_by_partial_link_text('India')
+        elem = driver.find_element_by_partial_link_text("India")
     except NoSuchElementException:
-        #code for unfriending
-        print("Being Unfriended:  "+friend)
-        div = driver.find_elements_by_css_selector("div[class='_2yfv _2yfv FriendButton']")
+        # Code for unfriending
+        print("Being Unfriended:  " + friend)
+        div = driver.find_elements_by_css_selector(
+            "div[class='_2yfv _2yfv FriendButton']"
+        )
         try:
-            links = div[0].find_elements_by_tag_name('a')
+            links = div[0].find_elements_by_tag_name("a")
             button = links[0]
             button.click()
             sleep(2)
-            Unfriend_button = driver.find_element_by_link_text('Unfriend')
+            Unfriend_button = driver.find_element_by_link_text("Unfriend")
             Unfriend_button.click()
         except IndexError:
-            print(" Could'nt Unfriend: "+friend)
-    sleep(2)
-    
+            print(" Could'nt Unfriend: " + friend)
 
+    sleep(2)
 
 driver.quit()
